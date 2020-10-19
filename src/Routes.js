a63e3d07ca1cbd20aa89def7d513081f92df1eed
Leo Scott <leo@designanddev.co.uk>
2020-10-19 15:10:59 +0100

    routes and layout structures

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Login from './pages/login/login';
-
+import navbar from './layouts/navbar.jsx';
 
 export default () => (
 	<BrowserRouter>
         <Switch>
             <Route path="/" exact component={Login} />
+            <Route path="/nav" exact component={navbar} />
         </Switch>
 	</BrowserRouter>
 );
