7f8a10b2bd15ff54a9b2725117bc35b04fcec787
Leo Scott <leo@designanddev.co.uk>
2020-11-05 22:25:12 +0000

    modals showing

--- src/pages/home/sectors.js
+++ src/pages/home/sectors.js
@@ -10,11 +10,56 @@ import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material
 import { green, purple } from '@material-ui/core/colors';
 import CreateIcon from '@material-ui/icons/Create';
 
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
 const theme = createMuiTheme({
   palette: {
     primary: green,
   },
 });
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    marginBottom: '20px'
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const SectorsModal = () => {
+  return (
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">react-transition-group animates me.</p>
+          </div>
+        </Fade>
+      </Modal>
+  )
+}
 
 const ColorButton = withStyles((theme) => ({
   root: {
@@ -25,12 +70,6 @@ const ColorButton = withStyles((theme) => ({
     },
   },
 }))(Button);
-
-const useStyles = makeStyles((theme) => ({
-  margin: {
-    marginBottom: '20px'
-  },
-}));
 
 const Sectors = () => {
   
@@ -95,12 +134,40 @@ const Sectors = () => {
   ];
 
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <ColorButton  variant="contained" color="primary"  className={classes.margin}>
+      <ColorButton onClick={handleOpen} variant="contained" color="primary" className={classes.margin}>
         <AddBoxIcon style={{ color: '#fff' }}/>
       </ColorButton>
+            <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">react-transition-group animates me.</p>
+          </div>
+        </Fade>
+      </Modal>
       <DataGrid rows={listSectors} columns={columns} pageSize={5} checkboxSelection />
     </div>
     )
